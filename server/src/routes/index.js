0360344d4be9106592da198ea7d8574c9cbfb902
DarkHandler <anymus.w0rk@gmail.com>
2021-05-11 18:49:33 -0400

    v0.0.6 - Resumen de informacion agregado a apartado de reciclaje

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -8,8 +8,13 @@ router.get('/', async (req,res)=>{ //ruta principal
     res.render('visitante/inicio');
 });
 
-router.get('/reciclaje', async (req,res)=>{ //ruta principal    
-    res.render('visitante/reciclaje');
+//const datos = await pool.query('select s.nombre, count(co.rol) as veces_recicla from sector as s inner join subsector as subs on s.sector_id=subs.sector_id inner join vivienda as v on v.subsector_id=subs.subsector_id inner join corroboracion as co on v.rol=co.rol group by s.nombre;');
+
+router.get('/reciclaje', async (req,res)=>{ //ruta principal
+    const datos = await pool.query('SELECT DATE_FORMAT(fecha_reciclaje,  "%Y-%m-%d" )"X", COUNT(*) "Y" FROM corroboracion WHERE fecha_reciclaje BETWEEN DATE_SUB((SELECT MAX(fecha_reciclaje) FROM corroboracion),INTERVAL 1 MONTH) AND (SELECT MAX(fecha_reciclaje) FROM corroboracion) GROUP BY fecha_reciclaje');
+    const total = await pool.query('SELECT COUNT(*) "Total" FROM corroboracion WHERE fecha_reciclaje BETWEEN DATE_SUB((SELECT MAX(fecha_reciclaje) FROM corroboracion),INTERVAL 1 MONTH) AND (SELECT MAX(fecha_reciclaje) FROM corroboracion)');
+    const info = [datos,total];
+    res.render('visitante/reciclaje',{info});
 });
 
 module.exports = router;
